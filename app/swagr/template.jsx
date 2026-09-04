58fb4c58df2547c19e4f2dca688b7a67cac53ec8
braden-alt <braden@allyouneedpromos.com>
2026-09-04 00:45:41 -0500

    SWAGR-JOURNEY-003 mount virtual journey entry

--- app/swagr/template.jsx
+++ app/swagr/template.jsx
@@ -2,12 +2,14 @@
 
 import JourneyHub from './journey-hub';
 import JourneyCapabilityLaunchpad from './journey-capability-launchpad';
+import JourneyVirtualLaunchpad from './journey-virtual-launchpad';
 
 export default function SwagrTemplate({ children }) {
   return (
     <>
       <JourneyHub />
       <JourneyCapabilityLaunchpad />
+      <JourneyVirtualLaunchpad />
       <div id="swagr-main-experience">{children}</div>
     </>
   );
