c3a127659eedba0bd01f61578ea135eb841ea380
braden-alt <braden@allyouneedpromos.com>
2026-09-03 22:57:24 -0500

    SWAGR-JOURNEY-002 surface governed capability launchpad

--- app/swagr/template.jsx
+++ app/swagr/template.jsx
@@ -1,11 +1,13 @@
 'use client';
 
 import JourneyHub from './journey-hub';
+import JourneyCapabilityLaunchpad from './journey-capability-launchpad';
 
 export default function SwagrTemplate({ children }) {
   return (
     <>
       <JourneyHub />
+      <JourneyCapabilityLaunchpad />
       <div id="swagr-main-experience">{children}</div>
     </>
   );
